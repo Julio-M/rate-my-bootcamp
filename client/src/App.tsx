0844cdc047a24e4e06aa71cf68bd3c616823e8c1
Michael Chung <mikechung89@gmail.com>
2022-07-26 23:20:35 -0400

    first commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import './App.css';
+import Main from './components/Main'
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
+      <Main />
     </div>
   );
 }
